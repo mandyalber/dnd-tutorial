4a77fbafe8cf3fcb2c5631ff7d033f1b1bcce10c
Mandy Alber <mandy.alber@gmail.com>
2021-03-05 15:50:35 -0800

    update to work for touchscreens

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { useCallback, useState } from 'react';
 import cuid from 'cuid';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import TouchBackend from 'react-dnd-touch-backend';
 import update from 'immutability-helper';
 import Dropzone from './Dropzone';
 import './App.css';
 import ImageList from './ImageList';
+
+const isTouchDevice = () => {
+  if('ontouchstart' in window) {
+    return true;
+  }
+  return false;
+};
+
+const backendForDND = isTouchDevice() ? TouchBackend : HTML5Backend;
 
 function App() {
   const [images, setImages] = useState([]);
@@ -35,7 +45,7 @@ function App() {
     <div className="App">
       <h1 className="text-center">Drag and Drop Example</h1>
       <Dropzone onDrop={onDrop} accept={"image/*"} />
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={backendForDND}>
         <ImageList images={images} moveImage={moveImage} />
       </DndProvider>
     </div>
